75e1f4cc1ebea00a2ef4bfbef7a14f5c5caaea6d
TMN <tommymartin1234@gmail.com>
2024-05-19 10:33:05 -0600

    feat: link server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import "./App.css";
+import { baseUrl } from "./urls";
 
 function App() {
+  const fetchServer = async () => {
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchServer();
+  }, []);
+
   return (
     <div>
-      <h1 style={{color:"red"}}>Planf Online 2.0</h1>
+      <h1>Planf Online 2.0</h1>
     </div>
   );
 }
